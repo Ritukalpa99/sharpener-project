c601674e82d10bc22dcdea61fab3d68ba973203f
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-04-20 16:57:43 +0530

    Hard coded expenses

---
+++ react-project/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem({name}) {
+    return (
+        <>
+        <h2>Expense Items</h2>
+        <h4>Food Rs 10</h4>
+        <h4>Petrol Rs 100</h4>
+        <h4>Movies Rs 200</h4>
+        </> 
+    )
+}
+
+export default ExpenseItem;
