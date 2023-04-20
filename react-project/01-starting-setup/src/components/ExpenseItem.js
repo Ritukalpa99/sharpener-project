26494cfa706f5e09cc35e857de7b0aadfb01441b
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-04-20 18:14:56 +0530

    Dynamic data basics

--- react-project/01-starting-setup/src/components/ExpenseItem.js
+++ react-project/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+	const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 295.35;
+	const locationOfExpense = 'Delhi';
   return (
-    <div className='expense-item'>
-      <div>March 28th 2001</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$210</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{locationOfExpense}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
