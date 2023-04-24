2b2db9aa7b798be61c56572f5f64ee03d8475608
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-04-24 11:42:07 +0530

    new component task done

--- react-project/02-working-with-state/src/components/NewExpense/ExpenseForm.js
+++ react-project/02-working-with-state/src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,18 @@
-
+import { useState } from "react";
 const ExpenseForm = () => {
-    const titleChangeHandler = (event) => {
-        console.log(event.target.value);
-    }
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+    const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
   return (
     <form>
       <div>
@@ -12,11 +22,11 @@ const ExpenseForm = () => {
         </div>
         <div>
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div>
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2024-12-31" />
+          <input type="date" min="2019-01-01" max="2024-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div>
