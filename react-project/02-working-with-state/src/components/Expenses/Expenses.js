2b12673939b03a78e069575292b79f0fb19b8bde
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-04-25 18:14:46 +0530

    conditional rendering

--- react-project/02-working-with-state/src/components/Expenses/Expenses.js
+++ react-project/02-working-with-state/src/components/Expenses/Expenses.js
@@ -15,20 +15,26 @@ const Expenses = (props) => {
     return expenses.date.getFullYear().toString() === filteredYear;
   });
 
+  let expenseContent = <p>No Expenses Found</p>;
+  if (filteredExpenses.length === 1) {
+    expenseContent = "Only single Expense here. Please add more...";
+  } else if (filteredExpenses.length > 1) {
+    expenseContent = filteredExpenses.map((exp) => (
+      <ExpenseItem
+        key={exp.id}
+        title={exp.title}
+        amount={exp.amount}
+        date={exp.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filtereChangeHanlder}
       />
-      {filteredExpenses.map((exp) => (
-        <ExpenseItem
-          key={exp.id}
-          title={exp.title}
-          amount={exp.amount}
-          date={exp.date}
-        />
-      ))}
+      {expenseContent}
     </Card>
   );
 };
